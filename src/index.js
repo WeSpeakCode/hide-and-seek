75fe9cf747633fcebcd4a0a9e0c99cd40431b519
Ramaraj T <jsramraj@gmail.com>
2021-06-05 00:29:07 +0530

    add socket io and create rooms

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Phaser from 'phaser';
+import { io } from 'socket.io-client';
+
 import shipImg from './assets/ship.png';
 import playerSprite from './assets/player.png';
 import { movePlayer } from './movement'
 import { initAnimation, animateMovement } from './animations'
-
+import {
+    getQueryParameter,
+    getRandomString,
+    updateQueryParameter,
+} from './utils';
 import {
     PLAYER_SPRITE_HEIGHT,
     PLAYER_SPRITE_WIDTH,
@@ -14,8 +20,16 @@ import {
 } from './constants';
 
 const player = {};
+const otherPlayers = [];
 
 let pressedKeys = [];
+let socket;
+const room = getQueryParameter('room') || getRandomString(5);
+window.history.replaceState(
+    {},
+    document.title,
+    updateQueryParameter('room', room),
+);
 
 class MyGame extends Phaser.Scene {
     constructor() {
@@ -28,6 +42,27 @@ class MyGame extends Phaser.Scene {
             frameWidth: PLAYER_SPRITE_WIDTH,
             frameHeight: PLAYER_SPRITE_HEIGHT,
         });
+
+        socket = io(`localhost:3000?room=${room}`);
+        socket.on('move', ({ x, y }) => {
+            console.log('revieved move');
+
+        });
+        socket.on('moveEnd', () => {
+            console.log('revieved moveend');
+        });
+        socket.on('playerJoined', () => {
+            console.log('playerJoined');
+            var otherPlayer = {};
+            
+            otherPlayer.sprite = this.add.sprite(PLAYER_START_X, PLAYER_START_Y, 'player');
+            otherPlayer.sprite.displayHeight = PLAYER_HEIGHT;
+            otherPlayer.sprite.displayWidth = PLAYER_WIDTH;
+            this.load.spritesheet('otherPlayer', playerSprite, {
+                frameWidth: PLAYER_SPRITE_WIDTH,
+                frameHeight: PLAYER_SPRITE_HEIGHT,
+              });
+        });
     }
 
     create() {
@@ -37,7 +72,7 @@ class MyGame extends Phaser.Scene {
         player.sprite.displayHeight = PLAYER_HEIGHT;
         player.sprite.displayWidth = PLAYER_WIDTH;
 
-        console.log(this);        
+        console.log(this);
         initAnimation(this);
 
         this.input.keyboard.on('keydown', (e) => {
@@ -54,8 +89,12 @@ class MyGame extends Phaser.Scene {
         this.scene.scene.cameras.main.centerOn(player.sprite.x, player.sprite.y);
         let playerMoved = movePlayer(pressedKeys, player.sprite);
         if (playerMoved) {
+            socket.emit('move', { x: player.sprite.x, y: player.sprite.y });
             player.movedLastFrame = true;
         } else {
+            if (player.movedLastFrame) {
+                socket.emit('moveEnd');
+            }
             player.movedLastFrame = false;
         }
         animateMovement(pressedKeys, player.sprite);
